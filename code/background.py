676491534e69f98a0b6d3d7adabb58df9902fca4
MatheusSales <matheussalesdasilva1@gmail.com>
2026-03-20 21:08:24 -0300

    Changing menu

--- code/background.py
+++ code/background.py
@@ -3,10 +3,10 @@
 
 from Entity import Entity
 
-
 class Background(Entity):
-    def __init__(self):
-        pass
+
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
         pass
